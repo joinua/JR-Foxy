76e854a4013e8efc70a74670f2c8430b5b2a7124
Oleksandr Roshkevych <56656852+joinua@users.noreply.github.com>
2026-06-22 13:51:15 +0300

    fix: refresh profile Telegram snapshots on activity

--- app/handlers/collect_members.py
+++ app/handlers/collect_members.py
@@ -4,6 +4,7 @@ from aiogram import F, Router
 from aiogram.types import Message
 
 from app.core.db import upsert_call_member
+from app.services.profile_service import sync_telegram_user
 
 router = Router()
 
@@ -12,19 +13,7 @@ router = Router()
     F.chat.type.in_({"group", "supergroup"}),
     ~F.text.regexp(r"^[!/]\w+"),
 )
-
 async def collect_member(message: Message) -> None:
-    if not message.from_user:
-        return
-
-    await upsert_call_member(
-        user_id=message.from_user.id,
-        username=message.from_user.username,
-        first_name=message.from_user.first_name,
-        last_name=message.from_user.last_name,
-        last_seen=int(time.time()),
-    )
-
     user = message.from_user
     if not user or user.is_bot:
         return
@@ -36,3 +25,4 @@ async def collect_member(message: Message) -> None:
         last_name=user.last_name,
         last_seen=int(time.time()),
     )
+    await sync_telegram_user(user, create=False)
